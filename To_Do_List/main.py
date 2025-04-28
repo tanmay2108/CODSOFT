065e16d13ffe51465ca5cb370694d7674ab8a1ee
tanmay2108 <tanmaygaikwad2108@gmail.com>
2025-04-28 20:38:07 +0530

    Added Delete task fuction to tasks.py

--- To_Do_List/main.py
+++ To_Do_List/main.py
@@ -5,21 +5,26 @@ def main():
     while True:
         print("this is the to do list...")
         print("1. Add Task")
-        print("2. View task")
+        print("2. View tasks")
         print("3. Mark task as Done")
+        print("4. Delete a task")
         print("6. exit")
 
         choice=input("choose what you want you want to do...(1-3)")
         if choice== '1':
             tasks.addTask()
         elif choice == '2':
-            tasks.view_task()
+            tasks.view_all_tasks()
         elif choice == '3':
             tasks.view_all_tasks()
             task_number= utility.validate_task_number()
             tasks.is_completed(task_number)
+        elif choice == '4':
+            tasks.view_all_tasks()
+            task_number= utility.validate_task_number()
+            tasks.delete_task(task_number)
         elif choice == '6':
-            print("better luck next time...")
+            print("Make sure you have a Task...")
             break
         else:
             print("invalid input")
--- To_Do_List/tasks.py
+++ To_Do_List/tasks.py
@@ -5,16 +5,6 @@ def addTask():
     tasks.append({"task":task, "done":False})
     print("Tsak added.")
 
-def view_task():
-    if not tasks:
-        print("No task found ")
-        return
-    else:
-        print("Your Tasks:")
-        for idx, task in enumerate(tasks, start=1):
-            print(f"{idx}. {task}")
-        print()
-
 def view_all_tasks():
     if len(tasks) == 0:
         print("No tasks available.\n")
@@ -30,5 +20,12 @@ def is_completed(task_number):
         task = tasks[task_number - 1]
         task["done"] = True
         print(f"Task '{task['task']}' marked as completed!\n")
+    except IndexError:
+        print("Invalid task number.\n")
+
+def delete_task(task_number):
+    try:
+        task = tasks.pop(task_number - 1)
+        print(f"Task '{task['task']}' deleted!\n")
     except IndexError:
         print("Invalid task number.\n")
