f4df5129b4e5a5c2a757325cc23e1d932ef395b1
tanmay2108 <tanmaygaikwad2108@gmail.com>
2025-05-08 01:54:38 +0530

    Created a new folder for task 3 Random Password generator

---
+++ Password_generator/pass_generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def new_password(length):
+    characters=string.ascii_letters + string.digits + string.punctuation
+    password=''.join(random.choices(characters,k=length))
+    return password
+  
+def main():
+    print("This is the new Password Generator...")
+    length=int(input("Length of the password= "))
+    if length>0:
+        password=new_password(length)
+        print(f"The new Generated password is {password}")
+    else:
+        print("Enter a valid Length")
+    
+main()
